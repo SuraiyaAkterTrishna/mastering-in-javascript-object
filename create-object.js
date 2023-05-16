25692950002ba045eda41327ed5b65c0b2137fa8
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-16 16:46:38 +0600

    create object using object literals

---
+++ create-object.js
@@ -0,0 +1,8 @@
+// create object using object literals
+const player = {};
+player.name = "Chonchol chowdhury";
+player.speciality = "Movie, Natok actor";
+player.hero = function(){
+    console.log("make good movie");
+};
+console.log(player);
