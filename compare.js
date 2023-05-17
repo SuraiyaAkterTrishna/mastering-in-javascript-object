887aa302ee8658c20e090c85adb8a16d0ec62cb4
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-18 04:37:40 +0600

    compare two objects

--- compare.js
+++ compare.js
@@ -12,7 +12,7 @@ else{
 
 // 2. Second way
 
-const first = { a: 2, c: 3, d: 5 };
+/* const first = { a: 2, c: 3, d: 5 };
 const second = { c: 3, b: 5, a: 2 };
 
 function compareObject(first, second) {
@@ -29,3 +29,25 @@ function compareObject(first, second) {
 }
 const result = compareObject(first, second);
 console.log(result);
+ */
+
+const first = { a: 2, c: 3, b: 5 };
+const second = { c: 3, b: 5, a: 2 };
+
+function compareObject(first, second){
+    const firstKeys = Object.keys(first);
+    const secondKeys = Object.keys(second);
+    if(firstKeys.length === secondKeys.length){
+        for (const key of firstKeys) {
+            if(first[key] !== second[key]){
+                return false;
+            }
+        }
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+const compare = compareObject(first, second);
+console.log(compare);
