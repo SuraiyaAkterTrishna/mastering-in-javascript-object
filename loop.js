7c3b977ee23ca3d15ae4e7528cb93c869601ff29
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-18 03:24:13 +0600

    for in loop

--- loop.js
+++ loop.js
@@ -2,16 +2,23 @@ const numbers = [10, 20, 30, 40, 50];
 for (const number of numbers) {
     // console.log(number);
 }
-const bottle = {color: 'Yellow', price: 50, isCleaned: true, capacity: 1};
-const keys = Object.keys(bottle);
+// const bottle = {color: 'Yellow', price: 50, isCleaned: true, capacity: 1};
+/* const keys = Object.keys(bottle);
 // console.log(keys);
 for (const key of keys) {
     const value = bottle[key];
-    console.log(key, value);
+    console.log(key,value);
 
-}
+} */
 
 /* bottle.color
 bottle['color'] */
 const color = 'color'
 // console.log(bottle[color]);
+
+
+const bottle = {color: 'Yellow', price: 50, isCleaned: true, capacity: 1};
+for(const key in bottle){
+    const value = bottle[key];
+    console.log(value);
+}
