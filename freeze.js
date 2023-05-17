40a095ecbab9deaccaef9b440eb3d1d7608c0e25
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-17 19:41:42 +0600

    keys, values, entries

---
+++ freeze.js
@@ -0,0 +1,9 @@
+const bottle = {color: 'Yellow', price: 50, isCleaned: true, capacity: 1};
+const keys = Object.keys(bottle);
+// console.log(keys);
+
+const values = Object.values(bottle);
+// console.log(values);
+
+const pair = Object.entries(bottle);
+console.log(pair);
