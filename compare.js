1c1cbec8a5927b21242638277e6fdeb387d3a379
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-18 04:16:48 +0600

    compare object

--- compare.js
+++ compare.js
@@ -1,4 +1,5 @@
-const first = {a: 2};
+// 1. First way
+/* const first = {a: 2};
 const second = {a: 2};
 const firstString = JSON.stringify(first);
 const secondString = JSON.stringify(second);
@@ -7,4 +8,24 @@ if(firstString === secondString){
 }
 else{
     console.log("They are not same");
+} */
+
+// 2. Second way
+
+const first = { a: 2, c: 3, d: 5 };
+const second = { c: 3, b: 5, a: 2 };
+
+function compareObject(first, second) {
+  const firstKeys = Object.keys(first);
+  const secondKeys = Object.keys(second);
+  if (firstKeys.length === secondKeys.length) {
+    for (const key of firstKeys) {
+      if (first[key] !== second[key]) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
+const result = compareObject(first, second);
+console.log(result);
