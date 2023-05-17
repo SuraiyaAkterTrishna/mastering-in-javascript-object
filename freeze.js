5b6ccb82f288fff7c4c0b75aa02e90d11e68564d
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-18 02:34:11 +0600

    Object.freeze()

--- freeze.js
+++ freeze.js
@@ -1,16 +1,22 @@
 const bottle = {color: 'Yellow', price: 50, isCleaned: true, capacity: 1};
-const keys = Object.keys(bottle);
-console.log(keys);
+/* const keys = Object.keys(bottle);
+// console.log(keys);
 
 const values = Object.values(bottle);
-console.log(values);
+// console.log(values);
 
-const pair = Object.entries(bottle);
-console.log(bottle);
+const pair = Object.entries(bottle); */
+/* // console.log(bottle);
 Object.seal(bottle);
 delete bottle.price;
-console.log(bottle);
+// console.log(bottle);
 bottle.price = 1000;
-console.log(bottle);
+// console.log(bottle);
 bottle.height = 12;
+// console.log(bottle); */
+
+Object.freeze(bottle);
+bottle.price = 1000;
+bottle.hight = 12;
+delete bottle.isCleaned;
 console.log(bottle);
