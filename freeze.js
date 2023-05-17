25067a45d4ff4df70f2612b2dc1764452682b835
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-17 20:22:51 +0600

    seal

--- freeze.js
+++ freeze.js
@@ -1,9 +1,16 @@
 const bottle = {color: 'Yellow', price: 50, isCleaned: true, capacity: 1};
 const keys = Object.keys(bottle);
-// console.log(keys);
+console.log(keys);
 
 const values = Object.values(bottle);
-// console.log(values);
+console.log(values);
 
 const pair = Object.entries(bottle);
-console.log(pair);
+console.log(bottle);
+Object.seal(bottle);
+delete bottle.price;
+console.log(bottle);
+bottle.price = 1000;
+console.log(bottle);
+bottle.height = 12;
+console.log(bottle);
